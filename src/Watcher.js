088c02b1e93d1817a63d1deb3dfe6de44216ed8d
chuckfairy <charlieabeling@gmail.com>
2016-05-18 21:11:45 -0500

    css updates, watch archive option using node-zip

--- src/Watcher.js
+++ src/Watcher.js
@@ -6,15 +6,21 @@
  */
 "use strict";
 
-var Utils = require( __dirname + "/utils/Utils.js" );
-
-var EventDispatcher = require( __dirname + "/utils/EventDispatcher.js" );
+var Async = require( "async" );
 
 var Path = require( "path" );
 
 var FS = require( "fs" );
 
+var NodeZip = require( "node-zip" );
+
+var Moment = require( "moment" );
+
 var FSWebcam = require( __dirname + "/FSWebcam.js" );
+
+var Utils = require( __dirname + "/utils/Utils.js" );
+
+var EventDispatcher = require( __dirname + "/utils/EventDispatcher.js" );
 
 
 //Main Class
@@ -60,6 +66,8 @@ Watcher.prototype = {
 
     //Update times
 
+    startUpdateTime: Date.now(),
+
     lastUpdateTime: Date.now(),
 
     lastLocation: "",
@@ -90,12 +98,12 @@ Watcher.prototype = {
 
             scope.watches.push( location );
 
-            scope.checkWatches();
-
             scope.dispatch({
                 type: "update",
                 file: location
             });
+
+            scope.checkWatches();
 
             if( ! scope.opts.autoUpdate ) { return; }
 
@@ -122,7 +130,15 @@ Watcher.prototype = {
 
         var deleteAfter = scope.opts.deleteAfter;
 
+        var archive = scope.opts.archiveAfter;
+
         var wl = scope.watches.length;
+
+        if( archive && wl > archive ) {
+
+            return scope.archiveWatches();
+
+        }
 
         if( ! deleteAfter ) { return; }
 
@@ -130,18 +146,93 @@ Watcher.prototype = {
 
         var deleteWatch = scope.watches.shift();
 
-        FS.unlink( deleteWatch, function() {
+        scope.deleteWatch( deleteWatch );
+
+    },
+
+
+    //Archive zip
+
+    archiveWatches: function() {
+
+        var scope = this;
+
+        var Zip = new NodeZip();
+
+        var watches = scope.watches.splice( 0, scope.opts.archiveAfter );
+
+        var wl = scope.watches.length;
+
+        var dir = scope.opts.zipDir
+            ? scope.opts.zipDir
+            : scope.opts.dir;
+
+        var zipName = dir
+            + scope.opts.name
+            + scope.startUpdateTime
+            + "-"
+            + scope.lastUpdateTime
+            + ".zip";
+
+        function zipWatch( watch, callback ) {
+
+            FS.readFile( watch, function( err, data ) {
+
+                Zip.file( Path.basename( watch ), data );
+
+                scope.deleteWatch( watch );
+
+                callback();
+
+            });
+
+        }
+
+        Async.map( watches, zipWatch, function() {
 
             if( scope.opts.verbose ) {
 
-                console.log( "FILE DELETE " + deleteWatch );
+                console.log( "ARCHIVING WATCHES " + zipName );
+
+            }
+
+            var data = Zip.generate({
+                base64: false,
+                compression: "DEFLATE"
+            });
+
+            FS.writeFile( zipName, data, 'binary' );
+
+
+            //Rewrites
+
+            scope.startUpdateTime = Date.now();
+
+        });
+
+    },
+
+
+    //Delete watch
+
+    deleteWatch: function( location, callback ) {
+
+        var scope = this;
+
+        FS.unlink( location, function() {
+
+            if( scope.opts.verbose ) {
+
+                console.log( "FILE DELETE " + location );
 
             }
 
             scope.dispatch({
                 type: "delete",
-                file: deleteWatch
+                file: location
             });
+
+            callback && callback();
 
         });
 
@@ -202,11 +293,15 @@ Watcher.Defaults = {
 
     autoDelete: true,
 
-    deleteAfter: 10,
+    deleteAfter: false,
+
+    archiveAfter: 5,
 
     verbose: true,
 
-    dir: Path.resolve( __dirname, "..", "cache" ) + "/"
+    dir: Path.resolve( __dirname, "..", "cache" ) + "/",
+
+    zipDir: false
 
 };
 
